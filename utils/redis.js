14d2c0998292b02d1562fd4697395e1723fe9424
Tebogo Ndaba <dennistebogo@gmail.com>
2024-05-16 00:38:58 +0200

    Redis server

--- utils/redis.js
+++ utils/redis.js
@@ -1,52 +1,40 @@
 const redis = require('redis');
+const { promisify } = require('util');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 class RedisClient {
-  constructor () {
-    this.client = redis.createClient();
-
-    this.client.on('error', (err) => {
-      console.error('Redis client error:', err);
+  constructor() {
+    this.client = redis.createClient({
+      host: process.env.REDIS_HOST || 'localhost',
+      port: process.env.REDIS_PORT || 6379,
     });
+
+    this.client.on('error', (err) => console.error('Redis client not connected to the server:', err));
+    this.client.on('connect', () => console.log('Redis client connected to the server'));
+
+    this.get = promisify(this.client.get).bind(this);
+    this.set = promisify(this.client.set).bind(this);
+    this.del = promisify(this.client.del).bind(this);
+    this.exists = promisify(this.client.exists).bind(this);
   }
 
-  isAlive () {
+  isAlive() {
     return this.client.connected;
   }
 
-  async get (key) {
-    return new Promise((resolve, reject) => {
-      this.client.get(key, (err, reply) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(reply);
-        }
-      });
-    });
+  async getValue(key) {
+    return this.get(key);
   }
 
-  async set (key, value, duration) {
-    return new Promise((resolve, reject) => {
-      this.client.setex(key, duration, value, (err, reply) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(reply);
-        }
-      });
-    });
+  async setValue(key, value, duration) {
+    await this.set(key, value);
+    this.client.expire(key, duration);
   }
 
-  async del (key) {
-    return new Promise((resolve, reject) => {
-      this.client.del(key, (err, reply) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(reply);
-        }
-      });
-    });
+  async delKey(key) {
+    return this.del(key);
   }
 }
 
