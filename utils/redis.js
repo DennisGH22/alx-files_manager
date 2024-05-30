fb657727b32bd3aa9fa6e5ac3f2fb348eec8810e
Tebogo Ndaba <tebogondaba@proton.me>
2024-05-31 00:28:08 +0200

    Remove enviroment variables for the Redis server

--- utils/redis.js
+++ utils/redis.js
@@ -1,14 +1,8 @@
 const redis = require('redis');
-const dotenv = require('dotenv');
-
-dotenv.config();
 
 class RedisClient {
   constructor() {
-    this.client = redis.createClient({
-      host: process.env.REDIS_HOST || 'localhost',
-      port: process.env.REDIS_PORT || 6379,
-    });
+    this.client = redis.createClient();
 
     this.client.on('error', (err) => console.error('Redis client not connected to the server:', err));
   }
