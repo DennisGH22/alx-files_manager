11d796bddb52d14eb654a9652e25a4bc6e81eaab
Tebogo Ndaba <tebogondaba@proton.me>
2024-05-30 23:57:19 +0200

    Update code - remove redundant code

--- utils/redis.js
+++ utils/redis.js
@@ -1,5 +1,4 @@
 const redis = require('redis');
-const { promisify } = require('util');
 const dotenv = require('dotenv');
 
 dotenv.config();
@@ -12,28 +11,22 @@ class RedisClient {
     });
 
     this.client.on('error', (err) => console.error('Redis client not connected to the server:', err));
-    this.client.on('connect', () => console.log('Redis client connected to the server'));
-
-    this.get = promisify(this.client.get).bind(this);
-    this.set = promisify(this.client.set).bind(this);
-    this.del = promisify(this.client.del).bind(this);
-    this.exists = promisify(this.client.exists).bind(this);
   }
 
   isAlive() {
     return this.client.connected;
   }
 
-  async getValue(key) {
+  async get(key) {
     return this.get(key);
   }
 
-  async setValue(key, value, duration) {
+  async set(key, value, duration) {
     await this.set(key, value);
     this.client.expire(key, duration);
   }
 
-  async delKey(key) {
+  async del(key) {
     return this.del(key);
   }
 }
